9414066394cfdaab0ecb6fc7c95b2fac3a908d66
anikachurilova <nyuta.churilova@gmail.com>
2020-10-10 23:54:53 +0300

    added Slot class

--- src/Slot.java
+++ src/Slot.java
@@ -10,14 +10,14 @@ public class Slot {
         this.subject = subject;
         this.group = group;
     }
-    public void setInstructor(Teacher teacher) { this.teacher = teacher; }
-    public void setMeetingTime(PeriodTime meetingTime) { this.periodTime = meetingTime; }
+    public void setTeacher(Teacher teacher) { this.teacher = teacher; }
+    public void setPeriodTime(PeriodTime periodTime) { this.periodTime = periodTime; }
     public void setRoom(Room room){ this.room = room; }
     public int getId() { return id; }
-    public StudentGroup getDept() { return group; }
-    public Subject getCourse() { return subject; }
-    public Teacher getInstructor() { return teacher; }
-    public PeriodTime getMeetingTime() { return periodTime; }
+    public StudentGroup getGroup() { return group; }
+    public Subject getSudject() { return subject; }
+    public Teacher getTeacher() { return teacher; }
+    public PeriodTime getPeriodTime() { return periodTime; }
     public Room getRoom() { return room; }
     public String toString() {
         return "["+group.getName()+","+subject.getName()+","+room.getNumber()+","+teacher.getId()+","+periodTime.getId() +"]";
